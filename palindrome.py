f478bb2f16a1358294e3e72bdf15a8d86b510413
Kajal <kajalrbhosale1@gmail.com>
2022-11-28 14:09:14 +0530

    2 commit

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,11 @@
 # print the palindrome between given range
 def countPal():
-    count1 = 0
+    count= 0
     for i in range(10, 1000):
         if str(i) == str(i)[::-1]:
             print(i)
-            count1 += 1
-    print("Total Count:-", count1)
+            count += 1
+    print("Total Count:-", count)
 
 
 countPal()
